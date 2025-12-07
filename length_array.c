eaf398ca189bb08db211338a0594250e80efcf7e
vijetaarasanal14 <131380536+vijetaarasanal14@users.noreply.github.com>
2025-12-07 12:32:20 +0530

    Add files via upload

---
+++ length_array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a[] = {10, 20, 30, 40, 50};
+
+    int length = sizeof(a) / sizeof(a[0]);
+
+    printf("Length of integer array = %d\n", length);
+
+    return 0;
+}

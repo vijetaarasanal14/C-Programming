999463d92da26c9815aeb715cf64e8be02e34d76
vijetaarasanal14 <131380536+vijetaarasanal14@users.noreply.github.com>
2025-12-12 10:50:35 +0530

    Add files via upload

---
+++ Declare _and_initialise_array.c
@@ -0,0 +1,14 @@
+//Declare and initialise the array
+#include<stdio.h>
+int main(){
+
+int arr[5]={1,2,3,4,5};
+
+printf(" elements of array:");
+
+for(int i=0; i<5; i++){
+printf("%d ", arr[i]);
+}
+
+return 0;
+}
